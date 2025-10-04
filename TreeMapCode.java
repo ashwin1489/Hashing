59903aca292b78fc7d2c20f1a472b5db46782b9d
ashwin1489 <ashw5325@gmail.com>
2025-10-04 09:05:41 +0530

    tm

---
+++ TreeMapCode.java
@@ -0,0 +1,15 @@
+
+import java.util.TreeMap;
+
+public class TreeMapCode {
+        public static void main(String args[]){
+        TreeMap<String,Integer>tm = new TreeMap<>();
+        tm.put("India",100);
+        tm.put("China",150);
+        tm.put("Us",50);
+
+        System.out.println(tm);
+
+    }
+    
+}
