4df136983375cd95a1cb2f1641cd44d60275c094
ashwin1489 <ashw5325@gmail.com>
2025-10-06 08:36:03 +0530

    HSC

---
+++ HashSetCode.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class HashSetCode {
+    public static void main(String[] args) {
+        HashSet<Integer>set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(4);
+        set.add(2);
+        set.add(1);
+
+        System.out.println(set);
+
+
+        set.remove(2);
+        if(set.contains(2)){
+            System.out.println("set contains 2");
+        }else{
+            System.out.println("false");
+        }
+        // if(set.contains(3)){
+        //     System.out.println("set contains 3");
+        // }
+        System.out.println(set.size()); 
+    
+    }
+}
